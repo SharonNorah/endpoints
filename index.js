c7f8262e619ee2b7a2f39285e247317919505661
akolsharonnorah@gmail.com <akolsharonnorah@gmail.com>
2019-10-02 11:27:20 +0300

    added the put endpoint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi"); //module returns a class
 const express = require("express");
 const app = express(); //returns an object of type express, called app by convention
 app.use(express.json());
@@ -14,10 +15,10 @@ app.get("/api/interventions", (req, res) => {
   res.send(interventions); //return interventions array
 });
 app.post("/api/nterventions", (req, res) => {
-  if (!req.body.name || req.body.name.length > 3) {
+  const { error } = validateIntervention(req.body); //getting result.error
+  if (error) {
     //400 bad request
-    res,
-      status(400).send("name is required and should be minimum 3 characters");
+    res, status(400).send(result.error.details[0].message);
     return;
   }
   const intervention = {
@@ -27,6 +28,34 @@ app.post("/api/nterventions", (req, res) => {
   interventions.push(intervention);
   res.send(intervention);
 });
+app.put("/api/interventions/:id", (req, res) => {
+  //look for the intervention
+  //if doesn't exit return 404
+  const intervention = interventions.find(
+    c => c.id === parseInt(req.params.id)
+  );
+  if (!intervention)
+    res.status(404).send("the intervention with the given id was not found");
+  //validate
+  const { error } = validateIntervention(req.body); //getting result.error
+  if (error) {
+    //400 bad request
+    res, status(400).send(result.error.details[0].message);
+    return;
+  }
+  //update the intervention
+  intervention.name = req.body.name;
+  //return updated intervention to client
+  res.send(intervention);
+});
+function validateIntervention(intervention) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .require()
+  };
+  return Joi.validate(intervention, schema); //returns an object
+}
 app.get("/api/interventions/:id", (req, res) => {
   const intervention = interventions.find(
     c => c.id === parseInt(req.params.id)
